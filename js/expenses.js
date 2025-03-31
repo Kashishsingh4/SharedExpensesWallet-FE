825b50f2b6dc32456da058e7e14b8eb19e894aee
unknown <kashishsingh0409@gmail.com>
2025-04-01 03:13:11 +0530

    changes

--- js/expenses.js
+++ js/expenses.js
@@ -1,4 +1,4 @@
-// expenses.js
+
 $(document).ready(function () {
     const username = localStorage.getItem("username");
     if (username) {
@@ -11,12 +11,17 @@ $(document).ready(function () {
 
 function loadExpenses(username) {
     $.get("http://localhost:5000/expenses", { username: username }, function (response) {
-        let expensesHtml = '';
+        let pendingExpensesHtml = '';
+        let settledExpensesHtml = '';
+        let pendingExpensesExist = false;
+        let settledExpensesExist = false;
+
         if (response.expenses.length === 0) {
-            expensesHtml = '<p>No available expenses.</p>';
+            pendingExpensesHtml = '<p>No available expenses.</p>';
+            settledExpensesHtml = '<p>No settled expenses.</p>';
         } else {
             response.expenses.forEach(expense => {
-                expensesHtml += `
+                const expenseHtml = `
                     <div class="expense-item">
                         <p><strong>Description:</strong> ${expense.description}</p>
                         <p><strong>Amount:</strong> $${expense.amount}</p>
@@ -24,9 +29,24 @@ function loadExpenses(username) {
                         ${!expense.settled ? `<button class="settle-expense" data-id="${expense._id}">Settle Expense</button>` : '<p>Settled</p>'}
                     </div>
                 `;
+                if (!expense.settled) {
+                    pendingExpensesHtml += expenseHtml;
+                    pendingExpensesExist = true;
+                } else {
+                    settledExpensesHtml += expenseHtml;
+                    settledExpensesExist = true;
+                }
             });
         }
-        $("#expenses-list").html(expensesHtml);
+        if(!pendingExpensesExist){
+          pendingExpensesHtml = '<p>No pending expenses.</p>';
+        }
+        if(!settledExpensesExist){
+          settledExpensesHtml = '<p>No settled expenses.</p>';
+        }
+
+        $("#pending-expenses-list").html(pendingExpensesHtml);
+        $("#settled-expenses-list").html(settledExpensesHtml);
 
         $(".settle-expense").click(function () {
             const expenseId = $(this).data("id");
